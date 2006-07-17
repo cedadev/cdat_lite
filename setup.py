08efff4ebd3a79dcf62afec4f8212ffb4093710d
spascoe <spascoe@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2006-07-17 16:26:35 +0000

    Setup.py now uses distutils.  Therefore the standard option --home=<prefix> is respected.  See README for details.
    git-svn-id: http://proj.badc.rl.ac.uk/svn/badc/CDAT/extras/trunk/mini-install@987 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- setup.py
+++ setup.py
@@ -22,6 +22,9 @@ packages = ['cdms', 'cdtime', 'cdutil', 'genutil', 'regrid', 'Properties', 'xmgr
 import sys, os
 from glob import glob
 
+from distutils.core import setup
+from distutils.command.install import install
+
 prefix = sys.argv[-1]
 
 
@@ -30,17 +33,88 @@ def do_cmd(cmd):
     if os.system(cmd) != 0:
         raise RuntimeError, 'Command failed'
 
-# Configure libcdms
-do_cmd('cd ../libcdms ; '
-       './configure --prefix=%s --disable-drs --disable-hdf --disable-dods '
-       '--disable-ql --with-ncinc=%s --with-ncincf=%s --with-nclib=%s'
-        % (prefix, netcdf_incdir, netcdf_incdir, netcdf_libdir))
 
-# compile libcdms
-do_cmd('cd ../libcdms ; make db_util ; make cdunif')
+class MyInstall(install):
 
-# Build each package
-sys.argv = ['setup.py', 'build']
+    description = "Install minimal CDAT"
+
+    user_options = install.user_options + [
+        ('netcdf-prefix=', None,
+         'Location of your netcdf distribution'),
+        ('netcdf-include=', None,
+         'Location of your netcdf include directory.  Overrides --netcdf-prefix'),
+        ('netcdf-lib=', None,
+         'Location of your netcdf lib directory.  Overrides --netcdf-prefix'),
+        ('cdat-packages=', None,
+         'List of packages to install')
+        ]
+
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.netcdf_prefix = None
+        self.netcdf_include = None
+        self.netcdf_lib = None
+        self.cdat_packages = 'cdms,cdtime,cdutil,genutil,regrid,Properties,xmgrace,unidata'
+
+    def run(self):
+        self.buildLibcdms()
+        self.installPackages()
+
+        print '''
+Success.
+
+Now just add %s/lib/python to your PYTHONPATH to access cdms.
+''' % self.home
+
+    
+    def buildLibcdms(self):
+        if self.netcdf_prefix:
+            netcdf_incdir = '%s/include' % self.netcdf_prefix
+            netcdf_libdir = '%s/lib' % self.netcdf_prefix
+        else:
+            netcdf_incdir = '/usr/local/include'
+            netcdf_libdir = '/usr/local/lib'
+        if self.netcdf_include:
+            netcdf_incdir = self.netcdf_include
+        if self.netcdf_lib:
+            netcdf_libdir = self.netcdf_lib
+
+        self._system('cd ../libcdms ; '
+                     './configure --disable-drs --disable-hdf --disable-dods '
+                     '--disable-ql --with-ncinc=%s --with-ncincf=%s --with-nclib=%s'
+                     % (netcdf_incdir, netcdf_incdir, netcdf_libdir))
+        self._system('cd ../libcdms ; make db_util ; make cdunif')
+
+    def installPackages(self):
+        curdir = os.path.abspath('.')
+        for package in self.cdat_packages.split(','):
+            if package == 'unidata':
+                # Unfortunately unidata assumes it's being installed in the default
+                # path and tries to install the udunits data file there.
+                self._system('cd ../Packages/%s ;'
+                             'PYTHONPATH=%s python ../../mini-install/unidata_setup.py '
+                             'install --home=%s'
+                             % (package, curdir, self.home))
+            else:
+                self._system('cd ../Packages/%s ;'
+                             'PYTHONPATH=%s python setup.py install --home=%s'
+                             % (package, curdir, self.home))
+
+    def _system(self, cmd):
+        self.execute(os.system, (cmd,))
+
+
+
+
+
+setup(name = 'mini-cdat',
+      cmdclass = {'install': MyInstall}
+      )
+
+
+raise SystemExit
+
+# Install each package
 for package in packages:
     if package == 'unidata':
         # Unfortunately unidata assumes it's being installed in the default
