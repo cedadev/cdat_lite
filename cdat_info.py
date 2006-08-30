4a9e0ba70287e8e6c416b4fa51ff1a288468db05
spascoe <spascoe@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2006-08-30 12:03:34 +0000

    cdscan is now added to the egg inside a package called 'cdat_scripts'. It can be accessed by importing cdat_scripts.cdscan and running cdat_scripts.cdscan.main(argv), or using the pkg_resources entry point API.  This should mean the script will be automatically installed when running easy_install on the egg.
    git-svn-id: http://proj.badc.rl.ac.uk/svn/badc/CDAT/extras/branches/mini-install-using-setuptools/mini-install@1102 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cdat_info.py
+++
@@ -1,8 +0,0 @@
-"""
-This module sets some variables which are used by the setup.py
-scripts of individual packages.
-"""
-
-cdunif_include_directories = ['../../libcdms/include', '/usr/local/include']
-cdunif_library_directories = ['../../libcdms/lib', '/usr/local/lib']
-cdunif_libraries = ['cdms', 'netcdf']
--- setup.py
+++ setup.py
@@ -66,7 +66,22 @@ def makeExtension(name, package_dir=None, sources=None,
 
     return e
 
+def linkScripts(scripts=['cdscan']):
+    """In order to put cdat scripts in their own package they are symbolically
+    linked into the cdat_scripts directory.
+    """
+    for script in scripts:
+        src = os.path.abspath(os.path.join('libcdms/src/python', script))
+        dest = os.path.abspath(os.path.join('cdat_scripts', script+'.py'))
+        if not os.path.exists(dest):
+            os.symlink(src, dest)
+    
 
+
+#------------------------------------------------------------------------------
+
+linkScripts()
+    
 setup(name='cdat-lite',
       description="Climate Data Analysis tools, core components",
       long_description=long_description,
@@ -77,20 +92,21 @@ setup(name='cdat-lite',
       
       setup_requires = requires,
       install_requires = requires + ['setuptools>=0.6c1'],
-
+      
       packages = ['unidata', 'cdms', 'cdutil', 'xmgrace', 'genutil',
-                  'PropertiedClasses', 'regrid'],
+                  'PropertiedClasses', 'regrid', 'cdat_scripts'],
       py_modules = ['MV'],
       package_dir = {'': 'Packages/cdms',
-                      'unidata': 'Packages/unidata/Lib',
-                      'cdms': 'Packages/cdms/Lib',
-                      'cdutil': 'Packages/cdutil/Lib',
-                      'xmgrace': 'Packages/xmgrace/Lib',
-                      'genutil': 'Packages/genutil/Lib',
-                      'PropertiedClasses': 'Packages/Properties/Lib',
-                      'regrid': 'Packages/regrid/Lib',
-                      },
-
+                     'unidata': 'Packages/unidata/Lib',
+                     'cdms': 'Packages/cdms/Lib',
+                     'cdutil': 'Packages/cdutil/Lib',
+                     'xmgrace': 'Packages/xmgrace/Lib',
+                     'genutil': 'Packages/genutil/Lib',
+                     'PropertiedClasses': 'Packages/Properties/Lib',
+                     'regrid': 'Packages/regrid/Lib',
+                     'cdat_scripts': 'cdat_scripts'
+                     },
+      
       ext_modules = [
         makeExtension('cdtime'),
         makeExtension('unidata.udunits_wrap', 'unidata'),
@@ -105,166 +121,16 @@ setup(name='cdat-lite',
                                     'Packages/regrid/Src/regrid.c'])
         ],
 
-       # Since udunits.dat isn't in the Lib directory we use the data_files attribute
-       # to install data.
-       #include_package_data = True,
-       #package_data = {'unidata': ['Packages/unidata/Src/*.dat']},
-       data_files = [('unidata', ['Packages/unidata/Src/udunits.dat'])],
-
-       cmdclass = {'build_ext': MyBuild_ext}
-      )
-
-sys.exit()
-
-#--------------------------------------------------------------------------------
-
-setup (name = "cdtime",
-       description = "Time utilities",
-       version='3.2',
-       url = "http://cdat.sf.net",
-       setup_requires = ['Numeric>=23.0'],
-       install_requires = ['Numeric>=23.0'],
-       ext_modules = [Extension('cdtime',
-                       ['Packages/cdtime/Src/cdtimemodule.c'],
-                                include_dirs = ['Packages/cdtime/Include', 'libcdms/include'],
-                                library_dirs = ['libcdms/lib'],
-                                libraries = ['cdms', 'netcdf']
-                                )
-                      ],
-       cmdclass = {'build': MyBuild,
-                   'build_ext': MyBuild_ext}
-       )
-
-setup (name = "udunits",
-       version='1.0',
-       author='doutriaux1@llnl.gov',
-       description = "Python wrapping for UDUNITS package developped by UNIDATA",
-       url = "http://www-pcmdi.llnl.gov/software",
-       setup_requires = ['Numeric>=23.0'],
-       install_requires = ['Numeric>=23.0'],
-
-       packages = ['unidata'],
-       package_dir = {'unidata': 'Packages/unidata/Lib'},
-
-       # Since udunits.dat isn't in the Lib directory we use the data_files attribute
-       # to install data.
-       #include_package_data = True,
-       #package_data = {'unidata': ['Packages/unidata/Src/*.dat']},
-       data_files = [('unidata', ['Packages/unidata/Src/udunits.dat'])],
-
-       ext_modules = [
-         Extension('unidata.udunits_wrap',
-                   ['Packages/unidata/Src/udunits_wrap.c',
-                    'Packages/unidata/Src/utparse.c',
-                    'Packages/unidata/Src/utlib.c',
-                    'Packages/unidata/Src/utscan.c',
-                    ],
-                   include_dirs = ['Packages/unidata/Include']
-                   )
-         ],
-       cmdclass = {'build': MyBuild,
-                   'build_ext': MyBuild_ext}
-       )
-
-
-setup (name = "cdms",
-       version='3.2',
-       description = "Climate Data Management System",
-       url = "http://cdat.sf.net",
-       setup_requires = ['Numeric>=23.0'],
-       install_requires = ['Numeric>=23.0'],
-       packages = ['cdms'],
-       py_modules = ['MV'],
-       package_dir = {'cdms': 'Packages/cdms/Lib',
-                      'MV': 'Packages/cdms'},
-       ext_modules = [Extension('cdms.Cdunif',
-                                ['Packages/cdms/Src/Cdunifmodule.c'],
-                                include_dirs = ['Packages/cdms/Include', 'libcdms/include'],
-                                library_dirs = ['libcdms/lib'],
-                                libraries = ['cdms', 'netcdf'],
-                                ),
-                      Extension('cdms._bindex',
-                                ['Packages/cdms/Src/_bindexmodule.c',
-                                 'Packages/cdms/Src/bindex.c'],
-                                )
-                      ],
-       cmdclass = {'build': MyBuild,
-                   'build_ext': MyBuild_ext}
-       )
-
-setup (name = "cdutil",
-       author="PCMDI Software Team",
-       version='4.0',
-       description = "Utilities for climate data manipulation",
-       url = "http://cdat.sourceforge.net",
-       packages = ['cdutil'],
-       package_dir = {'cdutil': 'Packages/cdutil/Lib'},
-       cmdclass = {'build': MyBuild}
-      )
-
-
-#setup (name = "MV",
-#       version = '1.0',
-#       description="Alias for cdms.MV",
-#       url = "http://cdat.sf.net",
-#       namespace_packages = ['ndg', 'ndg.cdatlite'],
-#       py_modules=['ndg.cdatlite.MV'],
-#       package_dir = {'ndg.cdatlite.MV': 'Packages/cdms'}
-#       )
-
-setup (name = "xmgrace",
-       version='1.1',
-       author='doutriaux1@llnl.gov',
-       description = "wrapping package for xmgrace",
-       url = "http://www-pcmdi.llnl.gov/software",
-       packages = ['xmgrace'],
-       package_dir = {'xmgrace': 'Packages/xmgrace/Lib'},
-       cmdclass = {'build': MyBuild}
-      )
-
-setup (name = "cdat",
-       version='4.0',
-       author='PCMDI',
-       description = "General utilities for scientific computing",
-       url = "http://www-pcmdi.llnl.gov/software",
-       setup_requires = ['Numeric>=23.0'],
-       install_requires = ['Numeric>=23.0'],
-       packages = ['genutil'],
-       package_dir = {'genutil': 'Packages/genutil/Lib'},
-       ext_modules = [
-    Extension('genutil.array_indexing',
-              ['Packages/genutil/Src/array_indexing.c'],
-              include_dirs = ['libcdms/include'],
-              ),
-
-    ],
-       cmdclass = {'build': MyBuild, 'build_ext': MyBuild_ext}
-      )
-
-execfile(os.path.join('Packages/Properties/Lib', 'Properties_version.py'))
-setup (name = "PropertiedClasses",
-       version=version,
-       author="Paul F. Dubois",
-       description = "Properties",
-       maintainer = "Numerical Python Developers",
-       maintainer_email = "numpy-developers@lists.sourceforge.net",
-       url = "http://sourceforge.net/projects/numpy",
-       packages = ['PropertiedClasses'],
-       package_dir = {'PropertiedClasses': 'Packages/Properties/Lib'},
-       cmdclass = {'build': MyBuild, 'build_ext': MyBuild_ext}
-      )
-
-setup (name = "regrid",
-       version='3.3',
-       description = "Remap Package",
-       url = "http://www-pcmdi.llnl.gov/software",
-       setup_requires = ['Numeric>=23.0'],
-       install_requires = ['Numeric>=23.0'],
-       packages = ['regrid'],
-       package_dir = {'regrid': 'Packages/regrid/Lib'},
-       include_dirs = ['libcdms/include'],
-       ext_modules = [Extension('regrid._regrid', ['Packages/regrid/Src/_regridmodule.c']),
-                      Extension('regrid._scrip', ['Packages/regrid/Src/_scripmodule.c','Packages/regrid/Src/regrid.c'])],
-       cmdclass = {'build': MyBuild, 'build_ext': MyBuild_ext}       
+      # Since udunits.dat isn't in the Lib directory we use the data_files attribute
+      # to install data.
+      #include_package_data = True,
+      #package_data = {'unidata': ['Packages/unidata/Src/*.dat']},
+      data_files = [('unidata', ['Packages/unidata/Src/udunits.dat'])],
+      
+      entry_points = {
+        'console_scripts': ['cdscan = cdat_scripts:cdscan_main']
+        },
+      
+      cmdclass = {'build_ext': MyBuild_ext}
       )
 
