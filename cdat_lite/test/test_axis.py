dd9ebbbed9bd6a772259409c5c42b76d0439739a
spascoe <spascoe@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2007-10-12 10:23:29 +0000

    Added a test that I expect to fail on 64-bit systems.
    git-svn-id: http://proj.badc.rl.ac.uk/svn/badc/CDAT/extras/trunk/cdat_lite@2076 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

---
+++ cdat_lite/test/test_axis.py
@@ -0,0 +1,28 @@
+"""
+Test axis features of CDMS.
+
+@note: These tests expect to be invoked using nose.
+
+"""
+
+import tempfile, os
+import cdms
+import Numeric as N
+
+def test_createAxis():
+    # On 64-bit systems this test fails due to attempted long to int
+    # type conversion inside cdms
+
+    # Create a temporary file
+    fd, f = tempfile.mkstemp('.nc')
+    os.close(fd)
+    try:
+        # Create an axis in a temporary NetCDF file
+        d = cdms.open(f, 'w')
+        ax_d = N.arange(10)
+        ax = d.createAxis('x', ax_d)
+        assert isinstance(ax, cdms.axis.FileAxis)
+    finally:
+        d.close()
+        os.remove(f)
+        
