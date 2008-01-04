91c4ab673ea46f078a410fa2891066b3e783ae48
spascoe <spascoe@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2008-01-04 09:36:30 +0000

    Changed the way Numeric is detected (again).  This is needed to compile on bond.
    git-svn-id: http://proj.badc.rl.ac.uk/svn/badc/CDAT/extras/trunk/cdat_lite@2598 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- setup_util.py
+++ setup_util.py
@@ -60,8 +60,10 @@ class build_ext(build_ext_orig):
         # Non-option attributes used during run()
         self.build_base = self.distribution.command_obj['build'].build_base
         self.build_lib = self.distribution.command_obj['build'].build_lib
-        self.netcdf_incdir = os.path.abspath('%s/netcdf-install/include' % self.build_base)
-        self.netcdf_libdir = os.path.abspath('%s/netcdf-install/lib' % self.build_base)
+
+        self.netcdf_incdir, self.netcdf_libdir = self._findNetcdf()
+        #self.netcdf_incdir = os.path.abspath('%s/netcdf-install/include' % self.build_base)
+        #self.netcdf_libdir = os.path.abspath('%s/netcdf-install/lib' % self.build_base)
 
         # Option attributes
         self.include_dirs += [self.findNumericHeaders(), self.netcdf_incdir,
@@ -71,11 +73,55 @@ class build_ext(build_ext_orig):
 
     def run(self):
 
-        self._buildNetcdf()
+        #self._buildNetcdf()
         self._buildLibcdms()
 
         build_ext_orig.run(self)
 
+    def _findNetcdf(self):
+        """Find NetCDF libraries installed on the system or prompt the user.
+
+        Various heuristics are tried to find the NetCDF installation.
+        """
+
+        prefix = None
+        inc_dir = None
+        lib_dir = None
+
+        def isPrefix(prefix):
+            print ('Looking for NetCDF installation in %s ...' % prefix),
+            if (os.path.exists('%s/lib/libnetcdf.a' % prefix) and 
+                os.path.exists('%s/include/netcdf.h' % prefix)):
+                print 'yes'
+                return True
+            else:
+                print 'no'
+                return False
+
+        prefixes = ['/usr', '/usr/local', os.environ['HOME']]
+
+        # Try finding a common NetCDF prefix
+        for p in prefixes:
+            if isPrefix(p):
+                prefix = p
+                break
+        else:
+            # Try looking for ncdump
+            for ex_p in os.environ['PATH'].split(':'):
+                if os.path.exists('%s/ncdump' % ex_p):
+                    updir = os.path.dirname(ex_p.rstrip('/'))
+                    if isPrefix(updir):
+                        prefix = updir
+                        break
+
+        if not prefix:
+            print '''===================================================
+Please enter NetCDF installation prefix:''',
+            prefix = raw_input()
+            if not isPrefix(prefix):
+                raise ValueError('Cannot find NetCDF libraries')
+
+        return '%s/include' % prefix, '%s/lib' % prefix
 
     def _buildNetcdf(self):
         """Build the netcdf libraries."""
@@ -141,6 +187,16 @@ class build_ext(build_ext_orig):
             import Numeric
         except ImportError:
             raise ConfigError, "Numeric is not installed."
+
+        if Numeric.__version__ != '23.1':
+            #!TODO only do this test on 64bit
+            raise ConfigError("CDAT requires Numeric 23.1, version %s found" %
+                              Numeric.__version__)
+
+        if 'NUMERIC_INCLUDE' in os.environ:
+            inc = os.environ['NUMERIC_INCLUDE']
+            if os.path.exists(inc):
+                return inc
 
         try:
             from Numeric_headers import get_numeric_include
