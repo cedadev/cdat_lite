2f5bfdf7b091e5a3b9d328aeea64eed2c690a3a3
spascoe <spascoe@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2008-02-01 12:26:53 +0000

    A test that once seemed to fail but works now
    git-svn-id: http://proj.badc.rl.ac.uk/svn/badc/CDAT/extras/trunk/cdat_lite@3006 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

---
+++ cdat_lite/test/test_scratchpad.py
@@ -0,0 +1,19 @@
+"""
+An informal module for noting down things that might need testing.
+
+"""
+
+import pkg_resources, cdms
+
+def test_netcdf():
+    """
+    Some problems being encountered using cdms on Python2.5
+
+    """
+
+    f = pkg_resources.resource_filename('cdat_lite.test', 'tas_mo_clim.nc')
+    d = cdms.open(f)
+    v = d['climseas']
+    s = v[slice(0,1,None)]
+    assert s.shape == (1,45,72)
+
