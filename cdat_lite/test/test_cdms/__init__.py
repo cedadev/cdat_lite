0ea2701ec0facf724d9ad8392423c77b07a90cfc
spascoe <spascoe@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2008-02-01 15:18:40 +0000

    Added most tests from CDAT's cdms package.  Not fully integrated yet.
    git-svn-id: http://proj.badc.rl.ac.uk/svn/badc/CDAT/extras/trunk/cdat_lite@3007 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cdat_lite/test/test_cdms/__init__.py
+++ cdat_lite/test/test_cdms/__init__.py
@@ -1,12 +1,39 @@
-# From cdms/Test/makeError.py
-# We don't use the error marking infrastructure but some of the test data
-# is created here.
+"""
+Run tests taken from CDAT/Packages/cdms/Test.
 
-import MA
+"""
 
-NTIME = 3
-NLAT = 16
-NLON = 32
+from pkg_resources import resource_filename
+from unittest import TestCase
+import imp
 
-x = MA.arange(float(2*NTIME*NLAT*NLON))
-x.shape=(2,NTIME,NLAT,NLON)
+
+class CdTest(TestCase):
+    """
+    Run all tests taken from CDAT's cdms package
+    
+    """
+
+    @classmethod
+    def addTestModule(cls, module):
+        name = 'test_%s' % module
+        def f(self):
+            self.runTest(module)
+        f.__name__ = name
+        
+        setattr(cls, name, f)
+
+    @classmethod
+    def addTestModules(cls, modules):
+        for m in modules:
+            cls.addTestModule(m)
+    
+    def runTest(self, module):
+        imp.load_module(module, *imp.find_module('%s.%s' % (__name__, module)))
+        
+            
+CdTest.addTestModules(['cdtest01', 'cdtest02', 'cdtest03', 'cdtest04',
+                       'cdtest05', 'cdtest06', 'cdtest07', 'cdtest08',
+                       'cdtest09', 'cdtest10',
+                       'cdtest11', 'cdtest12', 'cdtest13', 'cdtest14'])
+
