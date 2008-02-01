260b87a6432e5a1dae9fa16ddb269e8ce765b564
spascoe <spascoe@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2008-02-01 12:22:58 +0000

    Removed a cdutil module that depends on vcs
    git-svn-id: http://proj.badc.rl.ac.uk/svn/badc/CDAT/extras/trunk/cdat_lite@3005 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- Packages/cdutil/Lib/__init__.py
+++ Packages/cdutil/Lib/__init__.py
@@ -1,7 +1,8 @@
 """Module cdutil contains miscellaneous routines for manipulating variables.
 """
 import region
-import continent_fill
+# This module is dependent on vcs so is removed from cdat_lite
+#import continent_fill
 from genutil.averager import averager, AveragerError, area_weights, getAxisWeight, getAxisWeightByName,__check_weightoptions
 from times import *
 from retrieve import WeightsMaker,  WeightedGridMaker, VariableConditioner, VariablesMatcher
