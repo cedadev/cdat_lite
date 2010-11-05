3b4a62a301bd3c3f5325b83f5a19aafde6d3f6da
spascoe <spascoe@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2010-11-05 14:16:22 +0000

    Minimal test failing on x86_64
    git-svn-id: http://proj.badc.rl.ac.uk/svn/badc/CDAT/extras/trunk/cdat_lite@7956 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- lib/cdat_lite/test/test_scratchpad.py
+++ lib/cdat_lite/test/test_scratchpad.py
@@ -4,7 +4,8 @@ An informal module for noting down things that might need testing.
 
 """
 
-import pkg_resources, cdms2 as cdms
+import pkg_resources, cdms2
+import numpy
 
 def test_netcdf():
     """
@@ -13,8 +14,19 @@ def test_netcdf():
     """
 
     f = pkg_resources.resource_filename('cdat_lite.test', 'tas_mo_clim.nc')
-    d = cdms.open(f)
+    d = cdms2.open(f)
     v = d['climseas']
     s = v[slice(0,1,None)]
     assert s.shape == (1,45,72)
 
+
+def test_slice():
+    """
+    Problems with slicing on x86_64.
+
+    """
+    a = numpy.array([0])
+    var = cdms2.createVariable(a)
+    # This fails
+    print var[:]
+    
