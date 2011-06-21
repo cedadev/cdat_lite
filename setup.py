4d8c6a73953091011a0aeb74b0bacd4254f06824
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2011-06-21 12:04:18 +0100

    Support for detecting NetCDF configuration from the nc-config command.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from ez_setup import use_setuptools
 use_setuptools()
 
 from setuptools import setup, Extension, find_packages
-from setup_util import build_ext, makeExtension, buildLibTree, copyScripts, with_netcdf4
+from setup_util import build_ext, makeExtension, buildLibTree, copyScripts, netcdf_config
 
 NDG_EGG_REPOSITORY = 'http://ndg.nerc.ac.uk/dist/'
 CDAT_LITE_URL = 'http://proj.badc.rl.ac.uk/ndg/wiki/CdatLite'
@@ -70,75 +70,75 @@ if sys.version_info[:3] < (2,5):
 
 # cdunif macros depend on the presence of NetCDF4 API
 cdunif_macros = []
-if not with_netcdf4:
+if not netcdf_config.with_netcdf4:
     cdunif_macros.append(('NONC4', None))
 
 #------------------------------------------------------------------------------
+try:
+    setup(name='cdat_lite',
+          description=description,
+          long_description=long_description,
+          version='%s%s' % (cdat_release, cdat_tag),
+          url = CDAT_LITE_URL,
+          download_url = NDG_EGG_REPOSITORY,
+          maintainer = 'Stephen Pascoe',
+          maintainer_email = 'Stephen.Pascoe@stfc.ac.uk',
+          license = CDAT_LICENCE_URL,
 
-setup(name='cdat_lite',
-      description=description,
-      long_description=long_description,
-      version='%s%s' % (cdat_release, cdat_tag),
-      url = CDAT_LITE_URL,
-      download_url = NDG_EGG_REPOSITORY,
-      maintainer = 'Stephen Pascoe',
-      maintainer_email = 'Stephen.Pascoe@stfc.ac.uk',
-      license = CDAT_LICENCE_URL,
+          classifiers = [
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Science/Research',
+            'License :: Other/Proprietary License',
+            'Operating System :: POSIX :: Linux',
+            'Topic :: Scientific/Engineering :: Atmospheric Science',
+            ],
 
-      classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'License :: Other/Proprietary License',
-        'Operating System :: POSIX :: Linux',
-        'Topic :: Scientific/Engineering :: Atmospheric Science',
-        ],
-        
 
-      dependency_links = [NDG_EGG_REPOSITORY],
-      install_requires = ['numpy'],
-      zip_safe = False,
-      
-      packages = find_packages('lib'),
-      package_dir = {'': 'lib'},
-      py_modules = ['cdat_info', 'MV2'],
-      
-      ext_modules = [
-        makeExtension('cdtime'),
-        makeExtension('unidata.udunits_wrap', 'unidata'),
-        makeExtension('cdms2.Cdunif', 'cdms2', macros=cdunif_macros),
-        Extension('cdms2._bindex',
-                  ['Packages/cdms2/Src/_bindexmodule.c',
-                   'Packages/cdms2/Src/bindex.c'],
-                  depends=['libcdms/config.status'],
-                  ),
-        makeExtension('genutil.array_indexing', 'genutil'),
-        Extension('regrid2._regrid', ['Packages/regrid2/Src/_regridmodule.c'],
-                  depends=['libcdms/config.status'],
-                  ),
-        Extension('regrid2._scrip', ['Packages/regrid2/Src/_scripmodule.c',
-                                     'Packages/regrid2/Src/regrid.c'],
-                  depends=['libcdms/config.status'],
-                  ),
-        ],
-      
-      # Since udunits.dat isn't in the Lib directory we use the data_files attribute
-      # to install data.
-      include_package_data = True,
-      #package_data = {'cdat_lite.clib': ['include/*', 'lib/*']},
-      data_files = [('unidata', ['Packages/unidata/Src/udunits.dat'])],
-      
-      entry_points = {
-        'console_scripts': ['cdscan = cdat_lite.scripts:cdscan_main',
-                            'cddump = cdat_lite.scripts:cddump_main',
-                            'convertcdms = cdat_lite.scripts:convertcdms_main',
-                            ]
-        },
+          dependency_links = [NDG_EGG_REPOSITORY],
+          install_requires = ['numpy'],
+          zip_safe = False,
 
-      test_suite = 'nose.collector',
-      
-      cmdclass = {'build_ext': build_ext}
-      )
+          packages = find_packages('lib'),
+          package_dir = {'': 'lib'},
+          py_modules = ['cdat_info', 'MV2'],
 
-    
+          ext_modules = [
+            makeExtension('cdtime'),
+            makeExtension('unidata.udunits_wrap', 'unidata'),
+            makeExtension('cdms2.Cdunif', 'cdms2', macros=cdunif_macros),
+            Extension('cdms2._bindex',
+                      ['Packages/cdms2/Src/_bindexmodule.c',
+                       'Packages/cdms2/Src/bindex.c'],
+                      depends=['libcdms/config.status'],
+                      ),
+            makeExtension('genutil.array_indexing', 'genutil'),
+            Extension('regrid2._regrid', ['Packages/regrid2/Src/_regridmodule.c'],
+                      depends=['libcdms/config.status'],
+                      ),
+            Extension('regrid2._scrip', ['Packages/regrid2/Src/_scripmodule.c',
+                                         'Packages/regrid2/Src/regrid.c'],
+                      depends=['libcdms/config.status'],
+                      ),
+            ],
 
+          # Since udunits.dat isn't in the Lib directory we use the data_files attribute
+          # to install data.
+          include_package_data = True,
+          #package_data = {'cdat_lite.clib': ['include/*', 'lib/*']},
+          data_files = [('unidata', ['Packages/unidata/Src/udunits.dat'])],
+
+          entry_points = {
+            'console_scripts': ['cdscan = cdat_lite.scripts:cdscan_main',
+                                'cddump = cdat_lite.scripts:cddump_main',
+                                'convertcdms = cdat_lite.scripts:convertcdms_main',
+                                ]
+            },
+
+          test_suite = 'nose.collector',
+
+          cmdclass = {'build_ext': build_ext}
+          )
+finally:
+    # Summerise the configuration at the end    
+    netcdf_config.print_report()
